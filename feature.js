f053bc0c6779fb4e6ee444ffeee24857efeff043
Sumithra-1011 <139350799+Sumithra-1011@users.noreply.github.com>
2024-07-30 16:35:21 +0530

    create new pages
    create new pages

---
+++ feature.js
@@ -0,0 +1,16 @@
+// navbar scrolling
+let header=document.querySelector('.nav_head')
+console.log(header)
+window.addEventListener('scroll',()=>{
+    if(window.scrollY>100){
+        header.classList.add('header-scrolled')
+    }else if(window.scrollY<=00){
+        header.classList.remove('header-scrolled')
+
+    }
+})
+let navbartoggler = document.querySelector('.navbar-toggler')
+console.log(navbartoggler)
+navbartoggler.addEventListener('click',()=>{
+    header.classList.add('navbar-click')
+})
